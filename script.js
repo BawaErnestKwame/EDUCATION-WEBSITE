5df356ea825c89175dd3befd23b6d2c59684f1d3
BawaErnestKwame <bawaernest926@gmail.com>
2024-08-16 23:34:50 +0000

    navbar update

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function showSidebar(){
+    const showSidebar = document.querySelector("#sidebar");
+    showSidebar.style.display = "block";
+}
+
+function closeSidebar(){
+    const closeSidebar = document.querySelector("#sidebar");
+    closeSidebar.style.display = "none"
+}
